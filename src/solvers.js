15b9f6ef3fb86e6b698c9517fbdd4efbc3fc0341
Steve Miller <ben.el.elyon@gmail.com>
2018-12-19 23:06:31 -0500

    Psuedocoded most of solution for nrooks

--- src/solvers.js
+++ src/solvers.js
@@ -13,6 +13,33 @@
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
+// base case - we hit the bottom-right position of the board (max row, max column)
+
+var board = new Board(4);
+
+var solution = 0;
+var checkWholeBoard = function(board, row) {
+// for each spot in row
+// for (var i = 0; i < board.length; i++)
+// place in col 0 <- the board is updated to have one piece on it
+  var columnConflictCheck = board.hasAnyColConflicts;
+  // check for conflict <- has any col or row conflict
+  if (columnConflictCheck) {
+    // if there is conflict, then go to next spot in row
+  } else {
+    /*
+    if no conflict
+    if nextRow !== (n - 1) <- if I'm on row 3, don't do recursion
+      checkWholeBoard(board, nextRow); <- this board will have a piece at [0,0];
+      nextRow++;
+    if nextRow === (n - 1) <- am I on the last row?
+      increment solution++
+    */
+  }
+  // if we find one solution, will the whole function stop? or can we keep it going?
+
+};
+
 
 
 window.findNRooksSolution = function(n) {
